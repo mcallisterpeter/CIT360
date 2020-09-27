a8b1a35bb4e5699a965dd181e0155cc5d6a09b69
mcallisterpeter <pmcalli@byui.edu>
2020-09-26 20:26:48 -0600

    completed the four collection types

--- collections/src/w02_collections.java
+++ collections/src/w02_collections.java
@@ -3,60 +3,73 @@ import java.util.*;
 public class w02_collections {
     public static void main(String[] args) {
 
-        System.out.println("--List--");
-        List list = new ArrayList();
-        list.add("1.0");
-        list.add("1.1");
-        list.add("1.2");
-        list.add("1.3");
-        list.add("1.4");
-        list.add("1.5");
-        list.add("1.2");
+        Scanner input = new Scanner(System.in);
 
+        //Example of a List Collection
+        List list = new ArrayList();
+        System.out.println("--List Example--");
+        System.out.println("Please enter five words to be saved in the List Collection");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter a word: ");
+            list.add(input.nextLine());
+        }
+
+        System.out.println("Here are the words you entered in the order that you entered them:");
         for (Object str : list) {
             System.out.println((String)str);
         }
 
-        System.out.println("--Set--");
+        //Example of a Set Collection
         Set set = new TreeSet();
-        set.add("1.0");
-        set.add("1.1");
-        set.add("1.2");
-        set.add("1.3");
-        set.add("1.4");
-        set.add("1.5");
-        set.add("1.2");
+        System.out.println("--Set Example--");
+        System.out.println("Please enter five words to be saved in the Set Collection.\n" +
+                "Hint, try entering the same word twice.");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter a word: ");
+            set.add(input.nextLine());
+        }
 
+        System.out.println("Here are the words you entered in alphabetical order.\n" +
+                "Note, any words you entered twice only show up once:");
         for (Object str : set) {
             System.out.println((String)str);
         }
 
-        System.out.println("--Queue--");
+        //Example of a Queue Collection
         Queue queue = new PriorityQueue();
-        queue.add("1.0");
-        queue.add("1.1");
-        queue.add("1.2");
-        queue.add("1.3");
-        queue.add("1.4");
-        queue.add("1.5");
-        queue.add("1.2");
+        System.out.println("--Queue Example--");
+        System.out.println("Please enter five words to be saved in the Queue Collection.");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter a word: ");
+            queue.add(input.nextLine());
+        }
 
+        System.out.println("Here are the words you entered in alphabetical order.");
         Iterator iterator = queue.iterator();
         while (iterator.hasNext()) {
             System.out.println(queue.poll());
         }
 
-        System.out.println("--Map--");
+        //Example of a Map Collection
         Map map = new HashMap();
-        map.put(1, "1.0");
-        map.put(2, "1.1");
-        map.put(3, "1.2");
-        map.put(4, "1.3");
-        map.put(5, "1.4");
-        map.put(6, "1.5");
-        map.put(7, "1.2");
+        System.out.println("--Map Example--");
+        System.out.println("Please enter five words to be saved in the Map Collection.\n" +
+                "This time you have the ability to choose what order the array outputs.\n" +
+                "Enter first the order, between 1 and 5, and then enter the word.\n" +
+                "Note, if you type the same order number twice, the first word of that\n" +
+                "order number will be replaced.");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter the order number: ");
+            int myOrder = input.nextInt();
+            input.nextLine();
+            System.out.print("Enter a word: ");
+            String myWord = input.nextLine();
+            System.out.println(myOrder + " " + myWord);
+            map.put(myOrder, myWord);
+        }
 
-        for (int i = 1; i <= 7; i++) {
+        System.out.println("Here are the words you entered in the order you specified.");
+        for (int i = 1; i <= 5; i++) {
             String result = (String)map.get(i);
             System.out.println(result);
         }
