308d497f5834eadc5aecddcef35887693c8bc2ac
mcallisterpeter <pmcalli@byui.edu>
2020-11-07 18:13:56 -0700

    finished program

--- threads_runnables_executors/src/w08_threads_runnables_executors.java
+++ threads_runnables_executors/src/w08_threads_runnables_executors.java
@@ -1,35 +1,27 @@
-import java.util.Random;
 
 public class w08_threads_runnables_executors implements Runnable {
 
-    private String name;
-    private int number;
-    private int sleep;
-    private int rand;
+    private String customer;
+    private String type;
+    private int size;
+    private long cookTime;
 
-    public w08_threads_runnables_executors( String name, int number, int sleep) {
-        this.name = name;
-        this.number = number;
-        this.sleep = sleep;
-
-        Random random = new Random();
-        this.rand = random.nextInt(1000);
+    public w08_threads_runnables_executors( String customer, String type, int size, long cookTime) {
+        this.customer = customer;
+        this.type = type;
+        this.size = size;
+        this.cookTime = cookTime;
     }
 
     public void run() {
-        System.out.println("Executing with these parameters: Name = " + name + " Number = "
-                + number + " Sleep = " + sleep + " Rand Number = " + rand + "\n\n");
+        System.out.println("\nStarting " + customer + "'s " + size + " in. " + type + " Pizza in the oven.\n");
 
-        for (int count = 1; count < rand; count++) {
-            if (count % number == 0) {
-                System.out.print(name + " is sleeping. ");
-                try{
-                    Thread.sleep(sleep);
-                } catch(InterruptedException e) {
-                    System.err.println(e.toString());
-                }
-            }
+        try{
+            System.out.println("\n" + customer + "'s pizza is still cooking...\n");
+            Thread.sleep(cookTime);
+        } catch(InterruptedException e) {
+            System.err.println(e.toString());
         }
-        System.out.println("\n\n" + name + " is done." + "\n\n");
+        System.out.println("\n" + customer + "'s " + size + " in. " + type +" Pizza is done.\n");
     }
 }
--- threads_runnables_executors/src/w08_threads_runnables_executors_execute.java
+++ threads_runnables_executors/src/w08_threads_runnables_executors_execute.java
@@ -1,21 +1,40 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class w08_threads_runnables_executors_execute {
     public static void main(String[] args) {
-        ExecutorService myService = Executors.newFixedThreadPool(3);
 
-        w08_threads_runnables_executors ds1 = new w08_threads_runnables_executors("Peter", 32, 1000);
-        w08_threads_runnables_executors ds2 = new w08_threads_runnables_executors("Todd", 42, 500);
-        w08_threads_runnables_executors ds3 = new w08_threads_runnables_executors("Richard", 38, 250);
-        w08_threads_runnables_executors ds4 = new w08_threads_runnables_executors("Karen", 45, 100);
-        w08_threads_runnables_executors ds5 = new w08_threads_runnables_executors("Louise", 36, 50);
+        //Assign Pizza Oven capacity
+        int ovenCap = 4;
 
-        myService.execute(ds1);
-        myService.execute(ds2);
-        myService.execute(ds3);
-        myService.execute(ds4);
-        myService.execute(ds5);
+        //Set Pizza Oven cook time in minutes
+        long ovenTime = TimeUnit.MINUTES.toMillis(1);
+
+        //Set Oven capacity in ExecutorService
+        ExecutorService myService = Executors.newFixedThreadPool(ovenCap);
+
+        w08_threads_runnables_executors pizza1 = new w08_threads_runnables_executors("Peter", "Pepperoni", 14, ovenTime);
+        w08_threads_runnables_executors pizza2 = new w08_threads_runnables_executors("Karen", "Cheese", 10, ovenTime);
+        w08_threads_runnables_executors pizza3 = new w08_threads_runnables_executors("Susan", "Hawaiian", 16, ovenTime);
+        w08_threads_runnables_executors pizza4 = new w08_threads_runnables_executors("Eva", "Hawaiian", 14, ovenTime);
+        w08_threads_runnables_executors pizza5 = new w08_threads_runnables_executors("Gavin", "Meat Lovers", 14, ovenTime);
+        w08_threads_runnables_executors pizza6 = new w08_threads_runnables_executors("Kayli", "Veggie", 12, ovenTime);
+        w08_threads_runnables_executors pizza7 = new w08_threads_runnables_executors("Louise", "Works", 10, ovenTime);
+        w08_threads_runnables_executors pizza8 = new w08_threads_runnables_executors("Richard", "Pepperoni", 16, ovenTime);
+        w08_threads_runnables_executors pizza9 = new w08_threads_runnables_executors("Todd", "Cheese", 14, ovenTime);
+        w08_threads_runnables_executors pizza10 = new w08_threads_runnables_executors("Gayle", "BBQ", 14, ovenTime);
+
+        myService.execute(pizza1);
+        myService.execute(pizza2);
+        myService.execute(pizza3);
+        myService.execute(pizza4);
+        myService.execute(pizza5);
+        myService.execute(pizza6);
+        myService.execute(pizza7);
+        myService.execute(pizza8);
+        myService.execute(pizza9);
+        myService.execute(pizza10);
 
         myService.shutdown();
     }
